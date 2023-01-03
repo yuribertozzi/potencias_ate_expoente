50db2bd1aa8ed73813c2f2214175e668454884aa
Yuri Silva <121751241+yuribertozzi@users.noreply.github.com>
2023-01-03 12:14:31 -0300

    Add files via upload

---
+++ potencias.py
@@ -0,0 +1,17 @@
+print()
+
+print("Olá. Diga-me até que expoente quer que eu eleve a base 2, que lhe darei a respectiva lista.")
+
+print()
+
+e = int(input("Digite o valor do expoente: "))
+
+i = 0
+
+print()
+
+while i <= e:
+	
+	print(2 ** i)
+		
+	i = i + 1
